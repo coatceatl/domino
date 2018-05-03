8ceeb77dbb6773ad6bba944f82d274b645d316dc
Oksana Tkachenko <oksana@crystax.net>
2018-05-03 11:31:50 +0300

    WIP, adds eventListener for button reset

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-let dice = Math.floor(Math.random() * 6) + 1;
+const dice = Math.floor(Math.random() * 6) + 1;
 
 document.querySelector('.top-box').textContent = dice;
+//const displayDice = document.querySelector('.top-box')
+
+document.querySelector('.reset').addEventListener('click', function() {
+  console.log(dice);
+});
