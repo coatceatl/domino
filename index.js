72b715b8c8c495fb6c998920a79b3ac0f444c9a2
Oksana Tkachenko <oksana@crystax.net>
2018-05-03 15:28:50 +0300

    changed css for buttons

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-//document.querySelector('.top-box').textContent = dice;
-//document.querySelectorAll('.dot').display = 'none';
 
 document.querySelector('.reset').addEventListener('click', function() {
   const top = Math.floor(Math.random() * 7);
@@ -7,10 +5,21 @@ document.querySelector('.reset').addEventListener('click', function() {
 
   const displayDiceTop = document.querySelector('.top-box');
   const displayDiceBottom = document.querySelector('.bottom-box');
-  displayDiceTop.style.display = 'block';
-  displayDiceBottom.style.display = 'block';
+  displayDiceTop.style.display = 'flex';
+  displayDiceBottom.style.display = 'flex';
   displayDiceTop.textContent = top;
   displayDiceBottom.textContent = bottom;
   console.log(top);
   console.log(bottom);
 });
+
+/*document.querySelector('.rotate-left').addEventListener('click', function() {
+  console.log('rotate-left');
+});
+
+document.querySelector('.rotate-right').addEventListener('click', function() {
+  console.log('rotate-right');
+});
+*/
+
+
